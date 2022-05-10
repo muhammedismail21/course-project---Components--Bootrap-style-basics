63665b70cd304946cf1bbae2e147f1db70538f06
Ismail Mohammed <muhammed.ismail21@gmail.com>
2022-05-10 12:37:06 -0400

    Added content to Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
-    new Recipe('Biryani','Hyderabadi dum biryani','https://cdn.shopify.com/s/files/1/0634/0421/2467/products/514LLZ3MEmL_1024x1024.jpg')
+    new Recipe('Biryani','Hyderabadi dum biryani.','https://cdn.shopify.com/s/files/1/0634/0421/2467/products/514LLZ3MEmL_1024x1024.jpg'),
+    new Recipe('Haleem','Hyderabadi - Ramadhan special haleem.','https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/a0gfmetdt7xyohrzm2fn')
   ]; 
 
   constructor() { }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('Basmati Rice', 1.00),
+    new Ingredient('Chicken with bones', 1.00),
+    new Ingredient('Yogurt', 0.25),
+    new Ingredient('Spices', 0.01),
+    new Ingredient('Crushed Wheat flour', 1.00),
+    new Ingredient('Lamb or Goat', 2.00) 
+  ];
   constructor() { }
 
   ngOnInit(): void {
